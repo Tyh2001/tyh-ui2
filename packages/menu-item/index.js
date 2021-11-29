51d18abebae6ccb260992ee77fe465411fbef356
unknown <you@example.com>
2021-11-29 11:16:00 +0800

    更新名称bug

--- packages/menu-item/index.js
+++ packages/menu-item/index.js
@@ -1,7 +1,7 @@
 import TyhMenuItem from './TyhMenuItem.vue'
 
 TyhMenuItem.install = app => {
-  Vue.component('TyhMenuItem', TyhMenuItem)
+  app.component('TyhMenuItem', TyhMenuItem)
 }
 
 export default TyhMenuItem
