0e1dc624882668a6bf4222671e1caa61ceee53d5
Tyh2001 <1469442737@qq.com>
2022-01-06 15:13:07 +0800

    更新新目录

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,6 @@
-<template>
-  <ul>
-    <li v-for="index in 10" :key="index">
-      {{ users[index + numList - 1] }}
-    </li>
-  </ul>
-
-  <tyh-button-group>
-    <tyh-button @click="pagePev">上一页</tyh-button>
-    <tyh-button @click="pageAdd">下一页</tyh-button>
-  </tyh-button-group>
-</template>
+<template></template>
 
 <script setup>
-import { computed, ref } from 'vue'
-const users = []
-for (let i = 0; i < 1000002; i++) {
-  users.push(`张三${i + 1}`)
-}
-
-const page = ref(0)
-const numList = computed(() => {
-  return page.value * 10
-})
-
-function pageAdd () {
-  if (page.value <= users.length / 10) {
-    page.value++
-  }
-}
-function pagePev () {
-  if (page.value !== 0) {
-    page.value--
-  }
-}
 
 </script>
 
