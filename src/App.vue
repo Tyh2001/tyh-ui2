e28cf4c7839da9e257f4733331bea368a7d504ea
Tyh2001 <1469442737@qq.com>
2022-01-17 12:16:13 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <tyh-switch v-model="value" openText="打开了" closeText="关闭了" />
-  <br />
   <tyh-switch
     v-model="value"
     openText="打开了"
     closeText="关闭了"
+    @change="onChange($event)"
   />
   <br />
+  <tyh-switch v-model="value" openText="打开了" closeText="关闭了" />
+  <!-- <br />
   <el-switch
     v-model="value"
     active-color="#13ce66"
     inactive-color="#ff4949"
     active-text="按月付费"
     inactive-text="按年付费"
-  />
+  /> -->
 </template>
 
 <script setup>
