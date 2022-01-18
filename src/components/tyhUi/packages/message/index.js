1b69f4f23d4ecbf81c1ec017b8be400ab681718d
Tyh2001 <1469442737@qq.com>
2022-01-18 11:49:50 +0800

    重新开发message组件

--- src/components/tyhUi/packages/message/index.js
+++ src/components/tyhUi/packages/message/index.js
@@ -1,17 +1,1 @@
-import { createVNode, render } from 'vue'
-import TyhMessage from './TyhMessage.vue'
-
-const divVNode = createVNode('div')
-render(divVNode, document.body)
-const div = divVNode.el
-
-const Message = options => {
-  const comVNode = createVNode(TyhMessage, options)
-  render(comVNode, div)
-
-  setTimeout(() => {
-    render(null, div)
-  }, options.time || 2000)
-}
-
-export default Message
+export { Message } from './src/Message.js'
