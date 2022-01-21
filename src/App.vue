3d2ef33cecbda483b2b5e7e07ac39ce278abba05
Tyh2001 <1469442737@qq.com>
2022-01-21 16:55:41 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <tyh-switch v-model="value1" />
-  <tyh-switch v-model="value1" />
-   <tyh-switch v-model="value2" closeText="关闭" openText="开启" />
+  <tyh-division position="left" :margin="0">这是左边文字</tyh-division>
+  <p>这是一段文字</p>
+  <tyh-division position="center" :margin="20">这是中间文字</tyh-division>
+  <p>这是一段文字</p>
+  <tyh-division position="right">这是右边文字</tyh-division>
 </template>
 
 <script setup>
-import {ref} from 'vue'
-const value2 = ref(true)
+
 </script>
 
 <style scoped>
