b7e993089d7c667325cdb3c0cdbc99fe7c0893a8
tyh666999 <1469442737@qq.com>
2022-01-28 17:48:25 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <tyh-calendar v-model="value" /><tyh-crumbs>
-    <tyh-crumbs-item to="/">首页</tyh-crumbs-item>
-    <tyh-crumbs-item>学员管理</tyh-crumbs-item>
-    <tyh-crumbs-item>文章管理</tyh-crumbs-item>
-    <tyh-crumbs-item>设置</tyh-crumbs-item>
-  </tyh-crumbs>
+
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const value = ref(new Date())
+
 </script>
