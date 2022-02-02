025015290d36148275e5eed00c155759677e290b
tyh666999 <1469442737@qq.com>
2022-02-02 22:31:17 +0800

    初始化目录

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
-  <tyh-button round @click="change">这个是按钮</tyh-button>
-  <!-- <tyh-button type="b">这个是按钮</tyh-button>
-  <tyh-button type="c" disabled>这个是按钮</tyh-button> -->
+
 </template>
 
 <script setup>
-import TyhButton from './page_demo/TyhButton.vue'
-function change () {
-  alert('你好')
-}
+
 </script>
 
 <style scoped>
