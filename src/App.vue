92a99f4c572a03d96c13abd3c3b26ed95372f7c0
Tyh2001 <1469442737@qq.com>
2022-02-07 16:41:17 +0800

    开发radio中

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <input type="radio" name="1" id="" />
-  <input type="radio" name="1" id="" />
-  <input type="radio" name="1" id="" />
-
-  <tyh-radio />
+  <input type="radio" v-model="radio" value="1" />是1
+  <input type="radio" v-model="radio" value="2" />是2
+  <input type="radio" v-model="radio" value="3" />是3
+  <br />
+  <tyh-radio v-model="radio2" value="是1">是1</tyh-radio>
+  <tyh-radio v-model="radio2" value="否2">是2</tyh-radio>
+  <tyh-radio v-model="radio2" value="是3">是3</tyh-radio>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+const radio = ref('1')
+const radio2 = ref('1')
 </script>
--- src/components/tyhUi/packages/radio/TyhRadio.vue
+++ src/components/tyhUi/packages/radio/TyhRadio.vue
@@ -1,6 +1,24 @@
-<template>123</template>
+<template>
+  <input type="radio" :value="value" @input="input" />
+  <span>
+    <slot />
+  </span>
+</template>
 
 <script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  },
+  value: String
+})
+const emits = defineEmits(['update:modelValue'])
+
+function input (evt) {
+  console.log(evt.target.value)
+  emits('update:modelValue', evt.target.value)
+}
 
 </script>
 
