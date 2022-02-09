028d0449b6a86d49037dc14b3d60ee01b2fec210
Tyh2001 <1469442737@qq.com>
2022-02-09 15:04:13 +0800

    更新radio组件样式问题

--- src/components/tyhUi/packages/radio/TyhRadio.vue
+++ src/components/tyhUi/packages/radio/TyhRadio.vue
@@ -3,35 +3,16 @@
     class="tyh-radio"
     :style="{ cursor: disabled ? 'no-drop' : 'pointer' }"
   >
-    <span
-      :class="[
-        'tyh-radio-o',
-        modelValue === label
-          ? disabled
-            ? 'tyh-radio-hig-disabled'
-            : 'tyh-radio-hig'
-          : '',
-        disabled ? 'tyh-radio-disabled' : 'tyh-radio-on',
-      ]"
-    >
+    <span :class="isClass">
       <input
         v-model="modelValue"
-        hidden
         type="radio"
         :value="label"
         :disabled="disabled"
         @input="input"
       />
     </span>
-    <span
-      :class="[
-        'tyh-radio-label',
-        {
-          'tyh-radio-disabled': disabled,
-        },
-      ]"
-      :style="[{ color: modelValue === label ? '#3a6ff4' : '#515a6e' }]"
-    >
+    <span class="tyh-radio-text" :style="isStyle">
       <slot />
     </span>
   </label>
@@ -51,6 +32,23 @@ const props = defineProps({
 const emits = defineEmits(['update:modelValue'])
 
 const input = evt => emits('update:modelValue', evt.target.value)
+
+const isClass = computed(() => {
+  return [
+    'tyh-radio-o',
+    props.modelValue === props.label
+      ? props.disabled ? 'tyh-radio-disabled' : 'tyh-radio-hig'
+      : ''
+  ]
+})
+
+const isStyle = computed(() => {
+  return [{
+    color: props.modelValue === props.label
+      ? props.disabled ? '#b6b5b5' : '#3a6ff4'
+      : '#333'
+  }]
+})
 </script>
 
 <style scoped src="./style/index.css"></style>
