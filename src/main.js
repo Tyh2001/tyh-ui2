597486087910588c4fc8c8a31eda4e90a844245b
Tyh2001 <1469442737@qq.com>
2022-02-14 12:06:22 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -11,27 +11,7 @@ import './components/tyhUi/style/index.css'
 import 'element3/lib/theme-chalk/index.css'
 import Element3 from 'element3'
 
-<<<<<<< HEAD
-// Sentry.init({
-//   app,
-//   dsn: "https://d00292ffeac649c496479d4b9c850791@o1141265.ingest.sentry.io/6199274",
-//   integrations: [
-//     new BrowserTracing({
-//       routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-//       tracingOrigins: ["localhost", "my-site-url.com", /^\//],
-//     }),
-//   ],
-//   // Set tracesSampleRate to 1.0 to capture 100%
-//   // of transactions for performance monitoring.
-//   // We recommend adjusting this value in production
-//   tracesSampleRate: 1.0,
-// });
-
-// http://localhost/tyh-ui2/public/index.php/index/index/index
-onerror = function (msg, url) {
-=======
 onerror = function (msg, url, row, col) {
->>>>>>> 111efd43661320ce4d8b34d07cadefb62e71be20
   console.log(msg)
   console.log(url)
 }
