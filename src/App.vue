8e29cc9ad72b24cacc01ef8a317cf67d1f456e01
Tyh2001 <1469442737@qq.com>
2022-02-18 16:56:00 +0800

    refactor: 删除没用的代码

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,7 @@
-<template>
-  <!-- 
-    multiple 是否允许多选
-    size 可见的行数
-  -->
-  <tyh-select v-model="value" placeholder="请选择">
-    <tyh-option
-      v-for="(item, index) in options"
-      :key="index"
-      :value="item.value"
-    >
-      {{ item.label }}
-    </tyh-option>
-  </tyh-select>
-
-  <el-select v-model="value" placeholder="请选择">
-    <el-option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value"
-    />
-  </el-select>
-</template>
+<template></template>
 
 <script setup>
 import { ref, nextTick, reactive, provide } from 'vue'
-const value = ref('选项1')
-const options = [
-  {
-    value: '选项1',
-    label: '上海'
-  },
-  {
-    value: '选项2',
-    label: '北京'
-  },
-  {
-    value: '选项3',
-    label: '杭州'
-  },
-  {
-    value: '选项4',
-    label: '天津'
-  },
-  {
-    value: '选项5',
-    label: '大连'
-  }
-]
 // provide('newValue', '123')
 // 图片懒加载简单逻辑
 // nextTick(() => {
