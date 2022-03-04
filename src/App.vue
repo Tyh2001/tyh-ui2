f5748a2651f9fe3f2b79b77c30a426de974c38a4
Tyh2001 <1469442737@qq.com>
2022-03-04 17:20:53 +0800

    refactor: 优化一下代码片段和目录结构 :palm_tree:

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <template>
-  <tyh-card>
-    <template v-slot:title>主标题</template>
-    <template v-slot:subtitle>副标题</template>
-    <p>这是内容部分1</p>
-    <p>这是内容部分2</p>
-    <p>这是内容部分3</p>
-    <p>这是内容部分4</p>
-  </tyh-card>
+  <tyh-skeleton></tyh-skeleton>
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
