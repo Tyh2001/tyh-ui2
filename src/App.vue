04020799e3b2f44f7280ef5d8a95645c325cc89b
15227241216 <1469442737@qq.com>
2022-03-06 22:51:43 +0800

    pref: 优化 menu 组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
-  <tyh-menu mode="vertical" shadow theme="dark">
-    <!-- <tyh-menu-item url="/home">首页</tyh-menu-item>
-    <tyh-menu-item url="/abc">内容</tyh-menu-item>
+  <tyh-menu shadow mode="horizontal">
+    <tyh-menu-item to="/home">首页</tyh-menu-item>
+    <tyh-menu-item to="/abc">内容</tyh-menu-item>
     <tyh-menu-item prohibit>设置</tyh-menu-item>
     <tyh-menu-item prohibit>回收站</tyh-menu-item>
-    <tyh-menu-item></tyh-menu-item> -->
   </tyh-menu>
 
-  <tyh-menu mode="vertical" shadow theme="light">
-    <tyh-menu-item url="/home">首页</tyh-menu-item>
-    <tyh-menu-item url="/abc">内容</tyh-menu-item>
-    <tyh-menu-item>设置</tyh-menu-item>
-    <tyh-menu-item>回收站</tyh-menu-item>
-    <tyh-menu-item></tyh-menu-item>
+  <br />
+  <br />
+
+  <tyh-menu shadow mode="vertical" theme="light">
+    <!-- <tyh-menu shadow mode="horizontal"> -->
+    <template v-slot:left>
+      <tyh-button type="primary">主要按钮</tyh-button>
+    </template>
+
+    <template v-slot:right>
+      <tyh-input v-model="text1" />
+    </template>
+
+    <tyh-menu-item to="/home">首页</tyh-menu-item>
+    <tyh-menu-item to="/abc">内容</tyh-menu-item>
+    <tyh-menu-item prohibit>设置</tyh-menu-item>
+    <tyh-menu-item prohibit>回收站</tyh-menu-item>
   </tyh-menu>
+<!-- </div> -->
+
 </template>
 
 <script setup>
-
+const text1 = ''
 </script>
 
 <style>
