7cfc830ee64c15d335527bae1681779116fdff7b
15227241216 <1469442737@qq.com>
2022-03-07 21:01:15 +0800

    refactor: 更新错别字，删除注释代码

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <tyh-highlight>{{ code }}</tyh-highlight>
+    <!-- <tyh-highlight>{{ code }}</tyh-highlight> -->
     <!-- <highlightjs language="javascript" :code="code" /> -->
 
-    <pre v-tyh><code class="javascript">{{ code }}</code></pre>
+    <!-- <pre v-tyh><code class="javascript">{{ code }}</code></pre> -->
   </div>
 </template>
 
 <script setup>
-import TyhHighlight from './demo/tyh-highlight/TyhHighlight.vue'
-const code = `
-import { Message } from 'tyh-ui2'
-function change1 () {
-  Message({ message: '默认提示', type: 'default', round: true })
-}
-`
+// import TyhHighlight from './demo/tyh-highlight/TyhHighlight.vue'
+// const code = `
+// import { Message } from 'tyh-ui2'
+// function change1 () {
+//   Message({ message: '默认提示', type: 'default', round: true })
+// }
+// `
 </script>
