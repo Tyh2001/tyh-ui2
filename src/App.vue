29d5fdb679b3d98743c9689644d8b3d0225d1c29
Tyh2001 <1469442737@qq.com>
2022-03-09 15:50:53 +0800

    refactor: 添加 Current_Problems 目前的问题 文件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <button @click="show = true">Toggle render</button>
-  <tyh-dialog v-model="show" width="45%" title="这是标题" appendToBody>
-    哈哈
+  <tyh-dialog v-model="show" title="这是标题">
+    欢迎使用 tyh-ui 的 dialog 对话框！
     <template v-slot:footer>
-      <tyh-button type="primary">主要按钮</tyh-button>
+      <tyh-button type="primary" style="margin-right: 20px">
+        主要按钮
+      </tyh-button>
       <tyh-button type="success">成功按钮</tyh-button>
     </template>
   </tyh-dialog>
