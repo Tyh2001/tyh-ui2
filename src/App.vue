7baad8c9d8aad4f5e2ca275821594813c62376f4
15227241216 <1469442737@qq.com>
2022-03-12 20:53:34 +0800

    perf: 修复 link 组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <tyh-input v-model="text8" disabled clear />
+  <tyh-link url="" icon="tyh-ui-favorite-filling"></tyh-link>
+  <tyh-link url="" icon="tyh-ui-attachment" type="primary">链接二</tyh-link>
+  <tyh-link url="" icon="tyh-ui-customer-service" type="success"
+    >链接三</tyh-link
+  >
+  <tyh-link url="" icon="tyh-ui-filter" type="danger">链接四</tyh-link>
+  <tyh-link url="" icon="tyh-ui-good" type="warning">链接五</tyh-link>
 </template>
 
 <script setup>
--- src/packages/tyhUi/packages/link/src/TyhLink.vue
+++ src/packages/tyhUi/packages/link/src/TyhLink.vue
@@ -2,48 +2,23 @@
   <a
     :class="isClass"
     :style="{ textDecoration: underline ? 'none' : 'underline' }"
-    :href="prohibit ? 'javascript:void(0)' : url"
+    :href="prohibit ? null : url"
     :target="target"
   >
-    <tyh-icon
-      v-if="icon"
-      size="15"
-      :style="[
-        'margin-right: 2px',
-        { cursor: prohibit ? 'no-drop' : 'pointer' }
-      ]"
-      :icon="icon"
-      :color="THEME[type]"
-    />
+    <i v-if="icon" :class="['tyh-icon', `tyh-icon-${type}`, icon]" />
     <slot />
   </a>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { computed } from 'vue'
-import { THEME } from '../../../utils/theme'
-const props = defineProps({
-  type: {
-    type: String,
-    default: 'default',
-    validator(v) {
-      return ['default', 'primary', 'success', 'danger', 'warning'].includes(v)
-    }
-  },
-  prohibit: Boolean,
-  url: String,
-  underline: {
-    type: Boolean,
-    default: true
-  },
-  target: String,
-  icon: String
-})
-const isClass = computed(() => {
+import { prop } from './prop'
+const props = defineProps({ ...prop })
+const isClass = computed((): (string | object)[] => {
   return [
     'tyh-link',
     `tyh-link-${props.type}`,
-    { [`tyh-link-prohibit-${props.type}`]: props.prohibit }
+    { 'tyh-link-prohibit': props.prohibit }
   ]
 })
 </script>
