b2f4abac1f633e6758b7e950f2c2e6070582fa55
15227241216 <1469442737@qq.com>
2022-03-18 21:50:20 +0800

    chore: 更细 vitest 配置项

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {}
+  test: {
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**'
+    ],
+    environment: 'jsdom'
+  }
 })
