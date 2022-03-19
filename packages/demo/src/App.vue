cce3907dcfd7799d38eb6a0dcf98000fa58c9837
15227241216 <1469442737@qq.com>
2022-03-19 20:26:35 +0800

    refactor: 更新配置项

--- packages/demo/src/App.vue
+++ packages/demo/src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-    <h1>hello</h1>
-    <tyh-button>hello</tyh-button>
-  </div>
+  <h1 v-if="num === 1">hello</h1>
+  <h2 v-if="num === 2">hello</h2>
+  <h3 v-if="num === 3">hello</h3>
+  <h4 v-if="num === 4">hello</h4>
+  <h5 v-if="num === 5">hello</h5>
+  <h6 v-if="num === 6">hello</h6>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+defineProps({
+  num: Number
+})
+</script>
 
 <style scoped></style>
