a874fd25961b32a6ccc7335bae0b3c2aefe6c8df
15227241216 <1469442737@qq.com>
2022-03-26 14:04:55 +0800

    docs: 删除配置

--- packages/docs-new/vite.config.ts
+++ packages/docs-new/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Markdown from 'vite-plugin-md'
-const { resolve } = require('path')
+import { resolve } from 'path'
 
 export default defineConfig({
   base: './',
