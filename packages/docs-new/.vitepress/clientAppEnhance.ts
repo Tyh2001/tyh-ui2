51c731246a7f1c873f016f41a788ee5870a4187d
tyh2001 <1469442737@qq.com>
2022-04-19 17:56:45 +0800

    docs: 修复文档

--- packages/docs-new/.vitepress/clientAppEnhance.ts
+++
@@ -1,7 +0,0 @@
-import { defineClientAppEnhance } from '@vitepress/client'
-import tyhUi from 'tyh-ui2'
-import 'tyh-ui2/style/index.css'
-
-export default defineClientAppEnhance(({ app }) => {
-  app.use(tyhUi)
-})
