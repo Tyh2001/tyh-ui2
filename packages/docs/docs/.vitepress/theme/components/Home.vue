6ede2aa3d3f5a38fb09dffee74cfd28ecd0c3163
15227241216 <1469442737@qq.com>
2022-04-23 22:41:32 +0800

    docs: 更新文档

--- packages/docs/docs/.vitepress/theme/components/Home.vue
+++ packages/docs/docs/.vitepress/theme/components/Home.vue
@@ -1,206 +1,72 @@
 <template>
-  <section id="hero">
-    <h1 class="tagline">
-      <span class="accent">Tyh Ui</span>
-    </h1>
-    <p class="actions">
-      <a class="get-started" href="/components/install.html">快速开始</a>
-      <a class="setup" href="https://github.com/Tyh2001/tyh-ui">Github</a>
+  <div id="home">
+    <h3 class="title">Fighting Design</h3>
+    <h1 class="title_2">是一个用 TypeScript + Template 编写的 Vue3 组件库。</h1>
+
+    <p class="content">
+      <strong>Fighting Design</strong> 是一个简洁而又轻量级的
+      <strong>Vue3</strong>
+      组件库，为开发者、设计师和产品经历准备。有希望开发者可以借用
+      <strong>Fighting Design</strong>
+      的组件设计思想，在不久的将来，孕育出更高阶的组件库。
     </p>
-  </section>
+
+    <div class="action">
+      <a href="/demo/components/install.html">
+        <tyh-button type="primary">开始使用</tyh-button>
+      </a>
+      <tyh-button class="code" icon="tyh-ui-survey">
+        npm install filling-design
+      </tyh-button>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-section {
-  padding: 42px 32px;
-}
+<script lang="ts" setup></script>
 
-#hero {
-  padding: 96px 32px;
-  text-align: center;
-}
-
-.tagline {
-  font-size: 76px;
-  line-height: 1.25;
-  font-weight: 900;
-  letter-spacing: -1.5px;
-  max-width: 960px;
-  margin: 0px auto;
-}
-
-html:not(.dark) .accent,
-.dark .tagline {
-  background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff);
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-
-.description {
-  max-width: 960px;
-  line-height: 1.5;
-  color: var(--vt-c-text-2);
-  transition: color 0.5s;
-  font-size: 22px;
-  margin: 24px auto 40px;
-}
-
-.actions a {
-  font-size: 16px;
-  display: inline-block;
-  background-color: var(--vt-c-bg-mute);
-  padding: 8px 18px;
-  font-weight: 500;
-  border-radius: 8px;
-  transition: background-color 0.5s, color 0.5s;
-}
-
-.actions .get-started {
-  margin-right: 18px;
-}
-
-.actions .icon {
-  display: inline;
-  position: relative;
-  top: -1px;
-  margin-left: 2px;
-  fill: currentColor;
-  transition: transform 0.2s;
-}
-
-.actions .get-started:hover {
-  transition-duration: 0.2s;
-}
-
-.actions .get-started:hover .icon {
-  transform: translateX(2px);
-}
-
-.actions .get-started,
-.actions .setup {
-  color: var(--vt-c-text-code);
-}
-
-.actions .get-started:hover,
-.actions .setup:hover {
-  background-color: var(--vt-c-gray-light-4);
-  transition-duration: 0.2s;
-}
-
-.dark .actions .get-started:hover,
-.dark .actions .setup:hover {
-  background-color: var(--vt-c-gray-dark-3);
-}
-
-#special-sponsor {
-  border-top: 1px solid var(--vt-c-divider-light);
-  border-bottom: 1px solid var(--vt-c-divider-light);
-  padding: 12px 24px;
-  text-align: center;
-}
-
-#special-sponsor span {
-  color: var(--vt-c-text-2);
-  font-weight: 500;
-  font-size: 13px;
-  vertical-align: middle;
-  margin: 0 24px;
-}
-
-#special-sponsor img {
-  display: inline-block;
-  vertical-align: middle;
-  height: 36px;
-}
-
-.dark #special-sponsor img {
-  filter: grayscale(1) invert(1);
-}
-
-#highlights {
-  max-width: 960px;
-  margin: 0px auto;
-  color: var(--vt-c-text-2);
-}
-
-#highlights h2 {
-  font-weight: 600;
-  font-size: 20px;
-  letter-spacing: -0.4px;
-  color: var(--vt-c-text-1);
-  transition: color 0.5s;
-  margin-bottom: 0.75em;
-}
-
-#highlights p {
-  font-weight: 400;
-  font-size: 15px;
-}
-
-#highlights .vt-box {
-  background-color: transparent;
-}
-
-#sponsors {
-  max-width: 900px;
-  margin: 0px auto;
-}
-
-#sponsors h2 {
-  font-size: 20px;
-  font-weight: 600;
-  margin-bottom: 1em;
-}
-
-#sponsors .sponsor-container {
-  margin-bottom: 3em;
-}
-
-@media (max-width: 960px) {
-  .tagline {
-    font-size: 64px;
-    letter-spacing: -0.5px;
+<style lang="scss" scoped>
+#home {
+  padding: 50px;
+  box-sizing: border-box;
+  width: 1280px;
+  margin: auto;
+  .title {
+    font-weight: 600;
+    font-size: 32px;
+    color: rgb(17, 24, 39);
+    line-height: 100px;
   }
-  .description {
-    font-size: 18px;
-    margin-bottom: 48px;
+  .title_2 {
+    font-size: 4.5rem;
+    white-space: wrap;
+    line-height: 90px;
+    font-weight: 700;
+    color: rgb(17, 24, 39);
   }
-}
-
-@media (max-width: 768px) {
-  .tagline {
-    font-size: 48px;
-    letter-spacing: -0.5px;
+  .content {
+    margin-top: 40px;
+    font-size: 25px;
+    line-height: 36px;
+    display: inline-block;
+    width: 75%;
+    color: rgb(121, 129, 136);
+    strong {
+      color: rgb(68, 68, 68);
+    }
   }
-}
-
-@media (max-width: 576px) {
-  #hero {
-    padding: 64px 32px;
-  }
-  .description {
-    font-size: 16px;
-    margin: 18px 0 30px;
-  }
-  #special-sponsor img {
-    display: block;
-    margin: 2px auto 1px;
-  }
-  #highlights h3 {
-    margin-bottom: 0.6em;
-  }
-  #highlights .vt-box {
-    padding: 20px 36px;
-  }
-  .actions a {
-    margin: 0.5em 0;
-  }
-}
-
-@media (max-width: 370px) {
-  .tagline {
-    font-size: 36px;
+  .action {
+    margin-top: 30px;
+    display: flex;
+    align-items: center;
+    .tyh-button {
+      min-width: 130px;
+      height: 52px;
+      font-size: 18px;
+    }
+    .code {
+      margin-left: 17px;
+      font-size: 21px;
+    }
   }
 }
 </style>
