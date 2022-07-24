84c8f3271d250a2cc19aed71c87584684ceeb741
15227241216 <1469442737@qq.com>
2022-07-24 16:12:42 +0800

    chore: 更新构建配置

--- docs/docs/.vitepress/config.ts
+++ docs/docs/.vitepress/config.ts
@@ -1,5 +1,6 @@
 import { nav } from './utils/nav'
 import { sidebar } from './utils/sidebar'
+import { description } from './utils/description'
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
@@ -14,15 +15,14 @@ export default defineConfig({
       }
     ]
   ],
-  description:
-    'Fighting Design 是一个用 TypeScript + Template 编写的 Vue3 组件库。Fighting Design 是一个简洁而又轻量级的 Vue3 组件库，为开发者、设计师和产品经历准备。有希望开发者可以借用 Fighting Design 的组件设计思想，在不久的将来，孕育出更高阶的组件库。', // 页面的描述 添加到 mate 标签中
+  description,
   themeConfig: {
     nav,
     sidebar,
     socialLinks: [
       {
         icon: 'github',
-        link: 'https://github.com/FightingDesign/fighting-design'
+        link: 'https://github.com/Tyh2001/tyh-ui'
       }
     ],
     logo: 'https://tianyuhao.cn/images/tyh-ui/tyh-ui-logo.svg',
