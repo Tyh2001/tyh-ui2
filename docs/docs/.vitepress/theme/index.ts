73c8f9ddc9674c25d2b337cec1b764bb3f8c3ce9
15227241216 <1469442737@qq.com>
2022-07-24 15:48:27 +0800

    docs: 更新文档配置项

--- docs/docs/.vitepress/theme/index.ts
+++ docs/docs/.vitepress/theme/index.ts
@@ -1,8 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import tyhUi from '@tyh-ui2/components'
-// import tyhUi from 'tyh-ui2'
 import '@tyh-ui2/style'
-// import 'tyh-ui2/style/index.css'
 import './style/vitepress.scss'
 
 export default {
