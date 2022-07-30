c53edd4338156afc80803a07e1f815112642e829
田同学 <1469442737@qq.com>
2022-07-30 08:14:10 +0800

    docs：更新 start

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,9 +1,3 @@
 <script lang="ts" setup></script>
 
-<template>
-  <tyh-button loading>默认按钮</tyh-button>
-  <tyh-button loading type="primary">主要按钮</tyh-button>
-  <tyh-button loading type="success">成功按钮</tyh-button>
-  <tyh-button loading type="danger">危险按钮</tyh-button>
-  <tyh-button loading type="warning">警告按钮</tyh-button>
-</template>
+<template></template>
