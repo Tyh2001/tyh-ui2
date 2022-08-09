e2bcccc9a4a33335aaf302e70c588d201db5c548
15227241216 <1469442737@qq.com>
2022-08-09 23:44:14 +0800

    fest: 更新新版本

--- script/move-file.js
+++ script/move-file.js
@@ -3,18 +3,6 @@ const { copyFileSync, mkdirSync } = require('fs')
 mkdirSync('./dist/font')
 
 const FILE = [
-  {
-    input: './packages/style/font/tyh-icon.ttf',
-    outDir: 'dist/font/tyh-icon.ttf'
-  },
-  {
-    input: './packages/style/font/tyh-icon.woff',
-    outDir: 'dist/font/tyh-icon.woff'
-  },
-  {
-    input: './packages/style/font/tyh-icon.woff2',
-    outDir: 'dist/font/tyh-icon.woff2'
-  },
   { input: './README.md', outDir: 'dist/README.md' },
   { input: './packages/tyh-ui2/package.json', outDir: 'dist/package.json' },
   { input: './LICENSE', outDir: 'dist/LICENSE' }
